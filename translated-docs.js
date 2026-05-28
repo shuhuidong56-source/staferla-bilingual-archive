81206bd2db5b4f09eb4583e490813da45b116a4e
shuhuidong56-source <shuhuidong56@gmail.com>
2026-05-27 22:10:30 -0700

    Continue S1 translation to paragraph 663

--- translated-docs.js
+++ translated-docs.js
@@ -12,7 +12,7 @@ window.TRANSLATED_DOCS = {
   "S1": {
     "href": "./translated/S1.html",
     "status": "manual",
-    "paragraphs": 653
+    "paragraphs": 663
   },
   "S10": {
     "href": "./translated/S10.html",
